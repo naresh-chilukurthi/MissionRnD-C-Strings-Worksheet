cb19def592b56290314a63db68cfa310839c74dc
naresh117 <nareshchilukurthi@gmail.com>
2015-12-29 11:23:12 +0530

    Submit Commit

--- src/NumbertoStr.cpp
+++ src/NumbertoStr.cpp
@@ -1,5 +1,5 @@
 /*
-OVERVIEW: Given a float number ,Convert it into a string 
+OVERVIEW: Given a float number ,Convert it into a string
 
 INPUTS: A Float,Final string ,How many digits after decimal has to be displayed .
 Its zero for integers.
@@ -18,8 +18,53 @@ NOTES: Don't create new string.
 */
 
 #include <stdio.h>
-
-
+#include<malloc.h>
+int pow(int val,int exp)
+{   int i,sum=1;
+    for(i=1;i<=exp;i++)
+        sum=sum*val;
+    return sum;
+}
 void number_to_str(float number, char *str,int afterdecimal){
-	
+
+    int num,dec,rem,val,i=-1,start=0,j,power;char temp;
+    if(number<0)
+    {
+        ++i;
+        str[i]='-';
+        number=-number;
+        start=1;
+    }
+    num=(int)number;
+    val=(number*pow(10,afterdecimal));
+    dec=val%(pow(10,afterdecimal));
+
+    while(num>0)
+    {
+        i++;
+        rem=num%10;
+        str[i]=rem+'0';
+        num=num/10;
+
+
+    }
+    for(j=start;j<=(i+start)/2;j++)
+    {
+        temp=str[j];
+        str[j]=str[i-j+start];
+        str[i-j+start]=temp;
+        //printf("%c %c",temp,str[i-j]);
+    }
+    if(afterdecimal>0)
+    str[++i]='.';
+    power=pow(10,afterdecimal)/10;
+    //printf("%d %d ",dec,val%pow(10,afterdecimal));
+    while(power>0)
+    {
+        str[++i]=(dec/power)+'0';dec=dec-(dec/power)*power;
+        power=power/10;
+
+    }
+    str[++i]='\0';
+
 }
